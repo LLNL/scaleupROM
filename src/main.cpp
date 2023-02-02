d5597e804cd3369061652ab83dace688fb8eae1d
dreamer2368 <seung.chung@austin.utexas.edu>
2023-02-01 16:25:52 -0800

    minimal example of including an object.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "mfem.hpp"
+#include "test.hpp"
 
 using namespace mfem;
 
 int main(int argc, char* argv[])
 {
   printf("Hello World\n");
+
+  testObject tester(2);
+  printf("tester: %d\n", tester.a_);
   return 0;
 }
