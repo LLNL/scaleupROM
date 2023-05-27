a02fca12b672357fc86b689ff544778c8bd02295
Kevin Chung <chung28@llnl.gov>
2023-05-26 17:40:21 -0700

    multi-component test for stokes solver. verified.

---
+++ test/gmsh/stokes_multi_comp_dd_mms.cpp
@@ -0,0 +1,35 @@
+#include<gtest/gtest.h>
+#include "../mms_suite.hpp"
+
+using namespace std;
+using namespace mfem;
+using namespace mms::stokes;
+
+/**
+ * Simple smoke test to make sure Google Test is properly linked
+ */
+TEST(GoogleTestFramework, GoogleTestFrameworkFound) {
+   SUCCEED();
+}
+
+TEST(DDSerialTest, Test_convergence)
+{
+   config = InputParser("test.component.yml");
+   config.dict_["discretization"]["order"] = 2;
+   config.dict_["manufactured_solution"]["baseline_refinement"] = 0;
+   config.dict_["manufactured_solution"]["number_of_refinement"] = 2;
+   // TODO: add ROM capability for stokes solver.
+   config.dict_["main"]["use_rom"] = false;
+   CheckConvergence(1.0);
+
+   return;
+}
+
+int main(int argc, char* argv[])
+{
+   MPI_Init(&argc, &argv);
+   ::testing::InitGoogleTest(&argc, argv);
+   int result = RUN_ALL_TESTS();
+   MPI_Finalize();
+   return result;
+}
