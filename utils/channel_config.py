c5dc105d430c5c70eb3f2df971ddc5581e85f256
Kevin Chung <chung28@llnl.gov>
2023-06-13 16:33:29 -0700

    utils/config.py: base classes refactored.
    Component class have avail_face.

--- utils/channel_config.py
+++ utils/channel_config.py
@@ -1,166 +1,10 @@
-import numpy as np
-from copy import deepcopy
-import h5py
+from config import *
 
 def ManhattanDistance(loc1, loc2):
     assert(loc1.size == loc2.size)
     assert(len(loc1.shape) == 1)
     dist = loc1 - loc2
     return np.sum(np.abs(dist))
-
-class Component:
-    name = ''
-
-    num_face = -1
-    dim = 2
-    
-    ref_battr = []
-    face_map = {}       # {location of counterpart component: matching ref_battr}
-
-    def __init__(self):
-        return
-    
-    def switchBdrAttr(self, attr_dict, target_face, target_attr):
-        # switch upstream face index with the face which has attr 4.
-        attr_list = list(attr_dict.values())
-        face_list = list(attr_dict.keys())
-        uidx = attr_list.index(target_attr)
-        if (face_list[uidx] != target_face):
-            attr_dict[face_list[uidx]] = attr_dict[target_face]
-            attr_dict[target_face] = target_attr
-        return
-    
-
-class Empty(Component):
-    name = 'empty'
-
-    def __init__(self):
-        self.num_face = 4
-        self.ref_battr = [1, 2, 3, 4]
-        self.face_map = {(0, -1):  1,
-                         (1, 0):   2,
-                         (0, 1):   3,
-                         (-1, 0):  4}
-        return
-    
-    def DetermineGlobalAttr(self, uloc, loc, dloc, closure=False):
-        # determine no-slip wall faces of the last mesh.
-        faces = self.ref_battr.copy()
-        uface = self.face_map[tuple(uloc - loc)]
-        dface = self.face_map[tuple(dloc - loc)]
-
-        # downstream face index always has global bdr attr 2.
-        gbattr = {}
-        for face in faces:
-            attr = face - dface + 2
-            if (attr < 1): attr += self.num_face
-            if (attr > self.num_face): attr -= self.num_face
-            gbattr[face] = attr
-
-        # switch upstream face index with the face which has attr 4.
-        self.switchBdrAttr(gbattr, uface, 4)
-
-        gbattr.pop(uface)
-        if (not closure): gbattr.pop(dface)
-
-        return gbattr.values(), gbattr.keys()
-    
-class PipeHub(Component):
-    name = 'pipe-hub'
-
-    def __init__(self):
-        self.num_face = 5
-        self.ref_battr = [1, 2, 3, 4, 5]
-        self.face_map = {(0, -1):  1,
-                         (1, 0):   2,
-                         (0, 1):   3,
-                         (-1, 0):  4}
-        return
-    
-    def DetermineGlobalAttr(self, uloc, loc, dloc, closure=False):
-        # determine no-slip wall faces of the last mesh.
-        faces = self.ref_battr.copy()
-        uface = self.face_map[tuple(uloc - loc)]
-        dface = self.face_map[tuple(dloc - loc)]
-
-        # downstream face index always has global bdr attr 2.
-        gbattr = {}
-        for face in faces:
-            attr = face - dface + 2
-            if (attr < 1): attr += self.num_face
-            if (attr > self.num_face): attr -= self.num_face
-            gbattr[face] = attr
-
-        # Keep the attr 5.
-        self.switchBdrAttr(gbattr, 5, 5)
-
-        # switch upstream face index with the face which has attr 4.
-        self.switchBdrAttr(gbattr, uface, 4)
-
-        gbattr.pop(uface)
-        if (not closure): gbattr.pop(dface)
-
-        return gbattr.values(), gbattr.keys()
-    
-class Configuration:
-    prefix = 'config'
-    dim = 2
-
-    # reference component
-    comps = []
-    # reference port
-    ports = {}
-
-    # meshes in the global configuration. These are in fact pointers to reference component.
-    meshes = []
-    mesh_types = []
-    loc = np.array([[]], dtype=int)
-    bdr_data = np.array([[]], dtype=int)
-    if_data = np.array([[]], dtype=int)
-
-    def __init__(self):
-        return
-    
-    def addComponent(self, component):
-        assert(self.dim == component.dim)
-        self.comps += [component]
-        return
-    
-    def close(self):
-        raise RuntimeError('Abstract method Configuration.close()!')
-        return
-    
-    def getMeshConfigs(self):
-        mesh_configs = np.zeros([len(self.meshes), 6])
-        mesh_configs[:,:self.dim] = self.loc
-        return mesh_configs
-    
-    def save(self, filename):
-        with h5py.File(filename, 'w') as f:
-            # c++ currently cannot read datasets of string.
-            # change to multiple attributes, only as a temporary implementation.
-            grp = f.create_group("components")
-            grp.attrs["number_of_components"] = len(self.comps)
-            for k, comp in enumerate(self.comps):
-                grp.attrs["%d" % k] = comp.name
-
-            # component index of each mesh
-            grp.create_dataset("meshes", (len(self.mesh_types),), data=self.mesh_types)
-
-            # 3-dimension vector for translation / rotation
-            mesh_configs = self.getMeshConfigs()
-            grp.create_dataset("configuration", mesh_configs.shape, data=mesh_configs)
-
-            grp = f.create_group("ports")
-            grp.attrs["number_of_references"] = len(self.ports)
-            for k in range(len(self.ports)):
-                grp.attrs["%d" % k] = "port%d" % k
-
-            grp.create_dataset("interface", self.if_data.shape, data=self.if_data)
-
-            # boundary attributes
-            f.create_dataset("boundary", self.bdr_data.shape, data=self.bdr_data)
-        return
 
 class ChannelConfig(Configuration):
     prefix = 'channel.config'
@@ -170,7 +14,7 @@ class ChannelConfig(Configuration):
             self.comps = [Empty()]
         elif (start_comp == 'pipe-hub'):
             self.comps = [PipeHub()]
-        self.meshes = [self.comps[0]]
+        self.meshes = [deepcopy(self.comps[0])]
         self.mesh_types = [0]
         self.loc = np.zeros([1, self.dim], dtype=int)
         # self.loc[1, 0] = 1
@@ -180,6 +24,7 @@ class ChannelConfig(Configuration):
         #                           [1, 0, 1],
         #                           [3, 0, 3]])
         self.bdr_data = np.array([[4, 0, 4]])
+        self.meshes[0].avail_face.pop(4)
         # if (start_comp == 'pipe-hub'):
         #     self.bdr_data = np.append(self.bdr_data, [[5, 0, 5]], axis=0)
         
@@ -194,18 +39,10 @@ class ChannelConfig(Configuration):
         avail_faces = []
         avail_locs = []
 
-        if (self.loc.shape[0] == 1):
-            udir = (-1, 0)
-        else:
-            udir = tuple(self.loc[-2] - self.loc[-1])
-        uface = self.meshes[-1].face_map[udir]
-
-        for key, val in self.meshes[-1].face_map.items():
-            if (val == uface): continue
-
-            test_loc = self.loc[-1] + np.array(key)
+        for face, loc in self.meshes[-1].avail_face.items():
+            test_loc = self.loc[-1] + np.array(loc)
             if (self.isLocationAvailable(test_loc)):
-                avail_faces += [val]
+                avail_faces += [face]
                 avail_locs += [np.array(test_loc)]
 
         return avail_faces, avail_locs
@@ -235,7 +72,7 @@ class ChannelConfig(Configuration):
         assert(self.isLocationAvailable(new_loc))
 
         # upstream_face = self.meshes[-1].face_map[tuple(self.loc[-2] - self.loc[-1])]
-        downstream_face = self.meshes[-1].face_map[tuple(new_loc - self.loc[-1])]
+        d_face = self.meshes[-1].face_map[tuple(new_loc - self.loc[-1])]
 
         # no-slip wall boundary for the last mesh.
         m_idx = len(self.meshes) - 1
@@ -243,52 +80,34 @@ class ChannelConfig(Configuration):
             uloc = np.array([-1, 0]) + self.loc[0]
         else:
             uloc = self.loc[-2]
-        gbattrs, faces = self.meshes[-1].DetermineGlobalAttr(uloc, self.loc[-1], new_loc)
+        gbattrs, faces = self.DetermineGlobalAttr(-1, uloc, self.loc[-1], new_loc)
         for gbattr, face in zip(gbattrs, faces):
-            self.bdr_data = np.append(self.bdr_data, [[gbattr, m_idx, face]], axis=0)
+            self.appendBoundary(gbattr, m_idx, face)
+
+        # add new mesh and its type.
+        Configuration.addMesh(self, comp_idx, new_loc)
 
         # interface between the last mesh and the new mesh
-        c2_upstream = self.comps[comp_idx].face_map[tuple(self.loc[-1] - new_loc)]
-        if (downstream_face < c2_upstream):
-            interface = (self.meshes[-1].name, self.comps[comp_idx].name,
-                         downstream_face, c2_upstream)
-        else:
-            interface = (self.comps[comp_idx].name, self.meshes[-1].name,
-                         c2_upstream, downstream_face)
-            
-        # add the interface if not exists.
-        if interface not in self.ports:
-            self.ports[interface] = len(self.ports)
-        port = self.ports[interface]
-
-        if (downstream_face < c2_upstream):
-            if_data = [[m_idx, m_idx+1, downstream_face, c2_upstream, port]]
-        else:
-            if_data = [[m_idx+1, m_idx, c2_upstream, downstream_face, port]]
-
-        if (self.if_data.ndim == 1):
-            self.if_data = np.array(if_data)
-        else:
-            self.if_data = np.append(self.if_data, if_data, axis=0)
-
-        # add the new mesh and its location.
-        self.meshes += [self.comps[comp_idx]]
-        self.mesh_types += [comp_idx]
-        self.loc = np.append(self.loc, [new_loc], axis=0)
+        u_face2 = self.meshes[m_idx+1].face_map[tuple(self.loc[-2] - self.loc[-1])]
+        self.appendInterface(m_idx, m_idx+1, d_face, u_face2)
         return
     
     def close(self):
         m_idx = len(self.meshes) - 1
         new_loc = self.loc[-1] + (self.loc[-1] - self.loc[-2])
-        gbattrs, faces = self.meshes[-1].DetermineGlobalAttr(self.loc[-2], self.loc[-1], new_loc, True)
+        gbattrs, faces = self.DetermineGlobalAttr(-1, self.loc[-2], self.loc[-1], new_loc, True)
         for gbattr, face in zip(gbattrs, faces):
-            self.bdr_data = np.append(self.bdr_data, [[gbattr, m_idx, face]], axis=0)
+            self.appendBoundary(gbattr, m_idx, face)
 
         if not (self.allFacesClosed()):
             raise RuntimeError('ChannelConfig is not closed even after close()!')
         return
     
     def allFacesClosed(self):
+        for mesh in self.meshes:
+            if (len(mesh.avail_face) > 0):
+                return False
+
         mesh_faces = []
         for mesh in self.meshes:
             mesh_faces += [mesh.ref_battr.copy()]
@@ -303,8 +122,7 @@ class ChannelConfig(Configuration):
         closed = True
         for mesh_face in mesh_faces:
             if (len(mesh_face) != 0):
-                closed = False
-                break
+                return False
 
         return closed
     
@@ -335,10 +153,48 @@ class ChannelConfig(Configuration):
         config.close()
         config.save(filename)
         return
+    
+    def DetermineGlobalAttr(self, midx, uloc, loc, dloc, closure=False):
+        mesh = self.meshes[midx]
+        # determine no-slip wall faces of the last mesh.
+        faces = mesh.ref_battr.copy()
+        uface = mesh.face_map[tuple(uloc - loc)]
+        dface = mesh.face_map[tuple(dloc - loc)]
+
+        # downstream face index always has global bdr attr 2.
+        gbattr = {}
+        for face in faces:
+            attr = face - dface + 2
+            if (attr < 1): attr += mesh.num_face
+            if (attr > mesh.num_face): attr -= mesh.num_face
+            gbattr[face] = attr
+
+        # Keep the attr 5.
+        if 5 in gbattr:
+            self.switchBdrAttr(gbattr, 5, 5)
+
+        # switch upstream face index with the face which has attr 4.
+        self.switchBdrAttr(gbattr, uface, 4)
+
+        gbattr.pop(uface)
+        if (not closure): gbattr.pop(dface)
+
+        return gbattr.values(), gbattr.keys()
+    
+    def switchBdrAttr(self, attr_dict, target_face, target_attr):
+        # switch upstream face index with the face which has attr 4.
+        attr_list = list(attr_dict.values())
+        face_list = list(attr_dict.keys())
+        uidx = attr_list.index(target_attr)
+        if (face_list[uidx] != target_face):
+            attr_dict[face_list[uidx]] = attr_dict[target_face]
+            attr_dict[target_face] = target_attr
+        return
 
 if __name__ == "__main__":
     example = ChannelConfig('pipe-hub')
-    # example.GenerateAllConfigs(3, 0)
+    # example = ChannelConfig()
+    example.GenerateAllConfigs(3, 0)
 
     example.CreateRandomConfig(8, 'channel.9comp.h5')
 
---
+++ utils/config.py
@@ -0,0 +1,162 @@
+import numpy as np
+from copy import deepcopy
+import h5py
+
+class Component:
+    name = ''
+
+    num_face = -1
+    dim = 2
+    
+    ref_battr = []
+    face_map = {}       # {location of counterpart component: matching ref_battr}
+    face_map_inv = {}   # {matching ref_battr: location of counterpart component}
+    avail_face = {}     # same as face_map_inv
+
+    def __init__(self):
+        return
+
+class Empty(Component):
+    name = 'empty'
+
+    def __init__(self):
+        self.num_face = 4
+        self.ref_battr = [1, 2, 3, 4]
+        self.face_map = {(0, -1):  1,
+                         (1, 0):   2,
+                         (0, 1):   3,
+                         (-1, 0):  4}
+        for key, val in self.face_map.items():
+            self.face_map_inv[val] = key
+        self.avail_face = deepcopy(self.face_map_inv)
+        return
+    
+class PipeHub(Component):
+    name = 'pipe-hub'
+
+    def __init__(self):
+        self.num_face = 5
+        self.ref_battr = [1, 2, 3, 4, 5]
+        self.face_map = {(0, -1):  1,
+                         (1, 0):   2,
+                         (0, 1):   3,
+                         (-1, 0):  4}
+        for key, val in self.face_map.items():
+            self.face_map_inv[val] = key
+        self.avail_face = deepcopy(self.face_map_inv)
+        return
+    
+class Configuration:
+    prefix = 'config'
+    dim = 2
+
+    # reference component
+    comps = []
+    # reference port
+    ports = {}
+
+    # meshes in the global configuration. These are in fact pointers to reference component.
+    meshes = []
+    mesh_types = []
+    loc = np.array([[]], dtype=int)
+    bdr_data = np.array([[]], dtype=int)
+    if_data = np.array([[]], dtype=int)
+
+    def __init__(self):
+        return
+    
+    def addComponent(self, component):
+        assert(self.dim == component.dim)
+        self.comps += [component]
+        return
+    
+    def close(self):
+        raise RuntimeError('Abstract method Configuration.close()!')
+        return
+    
+    def isLocationAvailable(self, new_loc):
+        raise RuntimeError('Abstract method Configuration.isLocationAvailable!')
+        return
+    
+    def getMeshConfigs(self):
+        mesh_configs = np.zeros([len(self.meshes), 6])
+        mesh_configs[:,:self.dim] = self.loc
+        return mesh_configs
+    
+    def addMesh(self, comp_idx, new_loc):
+        assert(self.isLocationAvailable(new_loc))
+
+        # add new mesh and its type.
+        self.meshes += [deepcopy(self.comps[comp_idx])]
+        self.mesh_types += [comp_idx]
+
+        # add the new mesh's location.
+        self.loc = np.append(self.loc, [new_loc], axis=0)        
+        return
+    
+    def appendBoundary(self, gbattr, m_idx, face):
+        assert(face in self.meshes[m_idx].ref_battr)
+        if face in self.meshes[m_idx].avail_face:
+            self.meshes[m_idx].avail_face.pop(face)
+
+        self.bdr_data = np.append(self.bdr_data, [[gbattr, m_idx, face]], axis=0)
+        return
+    
+    def appendInterface(self, midx1, midx2, face1, face2):
+        assert(face1 in self.meshes[midx1].avail_face)
+        assert(face2 in self.meshes[midx2].avail_face)
+        if (midx1 < 0): midx1 += len(self.meshes)
+        if (midx2 < 0): midx2 += len(self.meshes)
+
+        self.meshes[midx1].avail_face.pop(face1)
+        self.meshes[midx2].avail_face.pop(face2)
+
+        if (face1 < face2):
+            interface = (self.meshes[midx1].name, self.meshes[midx2].name,
+                         face1, face2)
+        else:
+            interface = (self.meshes[midx2].name, self.meshes[midx1].name,
+                         face2, face1)
+            
+        # add the interface if not exists.
+        if interface not in self.ports:
+            self.ports[interface] = len(self.ports)
+        port = self.ports[interface]
+
+        if (face1 < face2):
+            if_data = [[midx1, midx2, face1, face2, port]]
+        else:
+            if_data = [[midx2, midx1, face2, face1, port]]
+
+        if (self.if_data.ndim == 1):
+            self.if_data = np.array(if_data)
+        else:
+            self.if_data = np.append(self.if_data, if_data, axis=0)
+        return
+    
+    def save(self, filename):
+        with h5py.File(filename, 'w') as f:
+            # c++ currently cannot read datasets of string.
+            # change to multiple attributes, only as a temporary implementation.
+            grp = f.create_group("components")
+            grp.attrs["number_of_components"] = len(self.comps)
+            for k, comp in enumerate(self.comps):
+                grp.attrs["%d" % k] = comp.name
+
+            # component index of each mesh
+            grp.create_dataset("meshes", (len(self.mesh_types),), data=self.mesh_types)
+
+            # 3-dimension vector for translation / rotation
+            mesh_configs = self.getMeshConfigs()
+            grp.create_dataset("configuration", mesh_configs.shape, data=mesh_configs)
+
+            grp = f.create_group("ports")
+            grp.attrs["number_of_references"] = len(self.ports)
+            for k in range(len(self.ports)):
+                grp.attrs["%d" % k] = "port%d" % k
+
+            grp.create_dataset("interface", self.if_data.shape, data=self.if_data)
+
+            # boundary attributes
+            f.create_dataset("boundary", self.bdr_data.shape, data=self.bdr_data)
+        return
