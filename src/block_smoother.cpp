63b5abb68798a0b1004236d3fc1a0236cfd91f87
Kevin Chung <chung28@llnl.gov>
2023-06-22 13:23:48 -0700

    BlockSmoother, BlockGSSmoother class for preconditioner.

---
+++ src/block_smoother.cpp
@@ -0,0 +1,147 @@
+// Copyright (c) 2010-2022, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+// Implementation of Bilinear Form Integrators
+
+#include "block_smoother.hpp"
+#include "etc.hpp"
+
+using namespace mfem;
+
+namespace mfem
+{
+
+BlockSmoother::BlockSmoother(const BlockMatrix &a)
+   : MatrixInverse(a),
+     oper(&a),
+     num_row_blocks(a.NumRowBlocks()),
+     num_col_blocks(a.NumColBlocks()),
+     row_offsets(a.RowOffsets()),
+     col_offsets(a.ColOffsets())
+{
+   assert(num_row_blocks == num_col_blocks);
+
+   diag_inv.SetSize(num_row_blocks);
+   diag_inv = NULL;
+   for (int b = 0; b < num_row_blocks; b++)
+   {
+      diag_inv[b] = oper->GetBlock(b, b).ToDenseMatrix();
+      diag_inv[b]->Invert();
+   }
+}
+
+BlockSmoother::~BlockSmoother()
+{
+   DeletePointers(diag_inv);
+}
+
+void BlockSmoother::SetOperator(const Operator &a)
+{
+   oper = dynamic_cast<const BlockMatrix*>(&a);
+   if (oper == NULL)
+   {
+      mfem_error("BlockSmoother::SetOperator : not a BlockMatrix!");
+   }
+   height = oper->Height();
+   width = oper->Width();
+}
+
+/// Matrix vector multiplication with Block GS Smoother.
+void BlockGSSmoother::Mult(const Vector &x, Vector &y) const
+{
+   if (!iterative_mode)
+   {
+      y = 0.0;
+   }
+   for (int i = 0; i < iterations; i++)
+   {
+      if (type != 2)
+      {
+         BlockGaussSeidelForw(x, y);
+      }
+      if (type != 1)
+      {
+         BlockGaussSeidelBack(x, y);
+      }
+   }
+}
+
+void BlockGSSmoother::BlockGaussSeidelForw(const Vector &x, Vector &y) const
+{
+   Vector xblockview, yblockview, tmp;
+
+   for (int i = 0; i < num_row_blocks; i++)
+   {   
+      tmp.SetSize(row_offsets[i+1] - row_offsets[i]);
+      tmp = 0.0;
+
+      for (int j = 0; j < num_col_blocks; j++)
+      {
+         if (i == j) continue;
+
+         if (!oper->IsZeroBlock(i, j))
+         {
+            yblockview.SetDataAndSize(
+               y.GetData() + col_offsets[j],
+               col_offsets[j+1] - col_offsets[j]);
+
+            // If i < j, yblockview is new y.
+            // If i > j, yblockview is old y.
+            oper->GetBlock(i, j).AddMult(yblockview, tmp, -1.0);
+         }
+      }
+
+      xblockview.SetDataAndSize(x.GetData() + row_offsets[i],
+                                row_offsets[i+1] - row_offsets[i]);
+      tmp += xblockview;
+
+      yblockview.SetDataAndSize(y.GetData() + row_offsets[i],
+                                row_offsets[i+1] - row_offsets[i]);
+      diag_inv[i]->Mult(tmp, yblockview);
+   }
+}
+
+void BlockGSSmoother::BlockGaussSeidelBack(const Vector &x, Vector &y) const
+{
+   Vector xblockview, yblockview, tmp;
+
+   for (int i = num_row_blocks-1; i >= 0; i--)
+   {   
+      tmp.SetSize(row_offsets[i+1] - row_offsets[i]);
+      tmp = 0.0;
+
+      for (int j = num_col_blocks-1; j >= 0; j--)
+      {
+         if (i == j) continue;
+
+         if (!oper->IsZeroBlock(i, j))
+         {
+            yblockview.SetDataAndSize(
+               y.GetData() + col_offsets[j],
+               col_offsets[j+1] - col_offsets[j]);
+
+            // If i > j, yblockview is new y.
+            // If i < j, yblockview is old y.
+            oper->GetBlock(i, j).AddMult(yblockview, tmp, -1.0);
+         }
+      }
+
+      xblockview.SetDataAndSize(x.GetData() + row_offsets[i],
+                                row_offsets[i+1] - row_offsets[i]);
+      tmp += xblockview;
+
+      yblockview.SetDataAndSize(y.GetData() + row_offsets[i],
+                                row_offsets[i+1] - row_offsets[i]);
+      diag_inv[i]->Mult(tmp, yblockview);
+   }
+}
+
+}
---
+++ test/test_block_smoother.cpp
@@ -0,0 +1,135 @@
+#include<gtest/gtest.h>
+#include "block_smoother.hpp"
+#include "etc.hpp"
+#include <fstream>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using namespace mfem;
+
+static const double threshold = 1.0e-14;
+
+/**
+ * Simple smoke test to make sure Google Test is properly linked
+ */
+TEST(GoogleTestFramework, GoogleTestFrameworkFound) {
+   SUCCEED();
+}
+
+TEST(BlockSmootherTest, BlockGSSmoother)
+{
+   const int nblocks = 4;
+   Array<int> block_offsets(nblocks+1);
+   block_offsets[0] = 0;
+   block_offsets[1] = 2;
+   block_offsets[2] = 3;
+   block_offsets[3] = 4;
+   block_offsets[4] = 5;
+   block_offsets.PartialSum();
+
+   Array2D<SparseMatrix *> mats(nblocks, nblocks);
+   for (int i = 0; i < nblocks; i++)
+      for (int j = 0; j < nblocks; j++)
+      {
+         mats(i, j) = new SparseMatrix(block_offsets[i+1] - block_offsets[i],
+                                       block_offsets[j+1] - block_offsets[j]);
+
+         for (int ii = 0; ii < block_offsets[i+1] - block_offsets[i]; ii++)
+            for (int jj = 0; jj < block_offsets[j+1] - block_offsets[j]; jj++)
+            {
+               double val = UniformRandom();
+               if (i == j) val *= 1.0e1;
+               mats(i, j)->Set(ii, jj, val);
+            }
+
+         mats(i, j)->Finalize();
+      }
+
+   BlockMatrix M(block_offsets), L(block_offsets), U(block_offsets);
+   BlockMatrix DL(block_offsets), DU(block_offsets);
+   for (int i = 0; i < nblocks; i++)
+      for (int j = 0; j < nblocks; j++)
+      {
+         M.SetBlock(i, j, mats(i, j));
+         if (i > j)
+         {
+            L.SetBlock(i, j, mats(i, j));
+            DL.SetBlock(i, j, mats(i, j));
+         }
+         else if (i < j)
+         {
+            U.SetBlock(i, j, mats(i, j));
+            DU.SetBlock(i, j, mats(i, j));
+         }
+         else
+         {
+            DL.SetBlock(i, j, mats(i, j));
+            DU.SetBlock(i, j, mats(i, j));
+         }
+      }
+
+   BlockGSSmoother gs(M);
+   DenseMatrix &DLinv(*(DL.CreateMonolithic()->ToDenseMatrix()));
+   DenseMatrix &DUinv(*(DU.CreateMonolithic()->ToDenseMatrix()));
+   DLinv.Invert();
+   DUinv.Invert();
+
+   Vector x(block_offsets.Last()), y0(block_offsets.Last());
+   Vector tmp(block_offsets.Last());
+   tmp = 0.0;
+   for (int k = 0; k < x.Size(); k++)
+   {
+      x(k) = UniformRandom();
+      y0(k) = UniformRandom();
+   }
+   
+   Vector fwy(block_offsets.Last()), fwy_true(block_offsets.Last());
+   fwy = y0; fwy_true = y0;
+
+   // true GS forward
+   tmp = 0.0;
+   U.AddMult(y0, tmp, -1.0);
+   tmp += x;
+   DLinv.Mult(tmp, fwy_true);
+
+   // GSSmoother forward
+   gs.BlockGaussSeidelForw(x, fwy);
+
+   double error = 0.0;
+   for (int k = 0; k < fwy.Size(); k++)
+      error = max(error, abs(fwy(k) - fwy_true(k)));
+   printf("GS forward error: %.5E\n", error);
+   EXPECT_TRUE(error < threshold);
+
+   Vector bwy(block_offsets.Last()), bwy_true(block_offsets.Last());
+   bwy = y0; bwy_true = y0;
+
+   // true GS forward
+   tmp = 0.0;
+   L.AddMult(y0, tmp, -1.0);
+   tmp += x;
+   DUinv.Mult(tmp, bwy_true);
+
+   // GSSmoother forward
+   gs.BlockGaussSeidelBack(x, bwy);
+
+   error = 0.0;
+   for (int k = 0; k < bwy.Size(); k++)
+      error = max(error, abs(bwy(k) - bwy_true(k)));
+   printf("GS backward error: %.5E\n", error);
+   EXPECT_TRUE(error < threshold);
+
+   return;
+}
+
+// TODO: add more tests from sketches/yaml_example.cpp.
+
+int main(int argc, char* argv[])
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   MPI_Init(&argc, &argv);
+   int result = RUN_ALL_TESTS();
+   MPI_Finalize();
+   return result;
+}
