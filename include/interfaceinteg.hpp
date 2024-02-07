11b7b1dee6e8c77c7a059c368e2f86a9bc95f878
Kevin Chung <chung28@llnl.gov>
2024-02-07 13:42:56 -0800

    instruction for template.

--- include/interfaceinteg.hpp
+++ include/interfaceinteg.hpp
@@ -199,7 +199,11 @@ public:
 
 // DGElasticityIntegrator
 // TODO(axel): you'll need to implement this class for dg interface for two different meshes.
-/* This is pretty much the copy version of MFEM DGElasticitityIntegrator */
+/* This is pretty much the copy version of MFEM DGElasticityIntegrator class,
+   which works for two different meshes.
+   For reference, the Poisson equation uses InterfaceDGDiffusionIntegrator,
+   which is the copy version of DGDiffusionIntegrator.
+ */
 class InterfaceDGElasticityIntegrator : public InterfaceNonlinearFormIntegrator
 {
 public:
