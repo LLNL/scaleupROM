4c5627124278a4cb327833e677ece8246c92702c
Axel Larsson <larsson4@llnl.gov>
2024-03-04 15:32:52 -0800

    Added config generation for componentwise training

--- utils/python/linelast_comp.py
+++ utils/python/linelast_comp.py
@@ -212,6 +212,100 @@ def LatticeCantilever(nx, ny):
         f.create_dataset("boundary", bdr_data.shape, data=bdr_data)
     return
 
+def ComponentWiseTrain():
+    n_mesh = 5
+    mesh_type = [0, 2, 1, 2, 1]
+    mesh_configs = np.zeros([n_mesh, 6])
+    mesh_configs[1,:] = [0., -5., 0., 0., 0., 0.]
+    mesh_configs[2,:] = [1., 0., 0., 0., 0., 0.]
+    mesh_configs[3,:] = [0., 1., 0., 0., 0., 0.]
+    mesh_configs[4,:] = [-5., 0., 0., 0., 0., 0.]
+
+    # interface data
+    # mesh1 / mesh2 / battr1 / battr2 / port_idx
+    if_data = np.zeros([4, 5])
+    if_data[0, :] = [0, 1, 1, 3, 0]
+    if_data[1, :] = [0, 2, 2, 4, 1]
+    if_data[2, :] = [0, 3, 3, 1, 2]
+    if_data[3, :] = [0, 4, 4, 2, 3]
+
+    # boundary attributes
+    # global_battr / mesh_idx / comp_battr
+    bdr_data = []
+
+    # applied loads and displacements
+    bdr_data += [[1, 1, 1]]
+    bdr_data += [[2, 2, 2]]
+    bdr_data += [[3, 3, 3]]
+    bdr_data += [[4, 4, 4]]
+
+    # homogenous neumann
+    bdr_data += [[5, 1, 2]]
+    bdr_data += [[5, 1, 4]]
+    bdr_data += [[5, 2, 1]]
+    bdr_data += [[5, 2, 3]]
+    bdr_data += [[5, 3, 2]]
+    bdr_data += [[5, 3, 4]]
+    bdr_data += [[5, 4, 1]]
+    bdr_data += [[5, 4, 3]]
+
+    bdr_data = np.array(bdr_data)
+    print(bdr_data.shape)
+
+    filename = "linelast.comp_train.h5"
+    with h5py.File(filename, 'w') as f:
+        # c++ currently cannot read datasets of string.
+        # change to multiple attributes, only as a temporary implementation.
+        grp = f.create_group("components")
+        grp.attrs["number_of_components"] = 3
+        grp.attrs["0"] = "joint2D"
+        grp.attrs["1"] = "rod2D_H"
+        grp.attrs["2"] = "rod2D_V"
+        # component index of each mesh
+        grp.create_dataset("meshes", (n_mesh,), data=mesh_type)
+        # 3-dimension vector for translation / rotation
+        grp.create_dataset("configuration", mesh_configs.shape, data=mesh_configs)
+
+        grp = f.create_group("ports")
+        grp.attrs["number_of_references"] = 4
+        grp.attrs["0"] = "port1"
+        grp.attrs["1"] = "port2"
+        grp.attrs["2"] = "port3"
+        grp.attrs["3"] = "port4"
+        grp.create_dataset("interface", if_data.shape, data=if_data)
+
+        port = grp.create_group("port1")
+        port.attrs["comp1"] = "joint2D"
+        port.attrs["comp2"] = "rod2D_V"
+        port.attrs["attr1"] = 1
+        port.attrs["attr2"] = 3
+        port.create_dataset("comp2_configuration", (6,), data=[0., -5., 0., 0., 0., 0.])
+
+        port = grp.create_group("port2")
+        port.attrs["comp1"] = "joint2D"
+        port.attrs["comp2"] = "rod2D_H"
+        port.attrs["attr1"] = 2
+        port.attrs["attr2"] = 4
+        port.create_dataset("comp2_configuration", (6,), data=[1., 0., 0., 0., 0., 0.])
+
+        port = grp.create_group("port3")
+        port.attrs["comp1"] = "joint2D"
+        port.attrs["comp2"] = "rod2D_V"
+        port.attrs["attr1"] = 3
+        port.attrs["attr2"] = 1
+        port.create_dataset("comp2_configuration", (6,), data=[0., 1., 0., 0., 0., 0.])
+
+        port = grp.create_group("port4")
+        port.attrs["comp1"] = "joint2D"
+        port.attrs["comp2"] = "rod2D_H"
+        port.attrs["attr1"] = 4
+        port.attrs["attr2"] = 2
+        port.create_dataset("comp2_configuration", (6,), data=[-5., 0., 0., 0., 0., 0.])
+
+        # boundary attributes
+        f.create_dataset("boundary", bdr_data.shape, data=bdr_data)
+    return
+
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
@@ -224,3 +318,5 @@ if __name__ == "__main__":
             nx = int(sys.argv[2])
             ny = int(sys.argv[3])
             LatticeCantilever(nx, ny)
+        elif name == "componentwise_train":
+            ComponentWiseTrain()
