c04bdc0fc3eaf5f923590a11135e02defc5e63ab
Cole Kendrick <kendrick6@llnl.gov>
2024-07-10 18:50:39 -0700

    Add stokes flow example

---
+++ examples/stokes/generate_configs.py
@@ -0,0 +1,19 @@
+import sys
+# Add path to root utils/python/ directory to find config scripts
+sys.path.insert(0, "../../../utils/python/")
+
+from config import Empty, ObjectInSpace
+from box_channel_config import BoxChannelConfig
+
+if __name__ == "__main__":
+    comp_list = {'empty': Empty(),
+                 'circle': ObjectInSpace('square-circle'),
+                 'square': ObjectInSpace('square-square'),
+                 'triangle': ObjectInSpace('square-triangle'),
+                 'star': ObjectInSpace('square-star'),}
+
+    example = BoxChannelConfig(2,2)
+    for name, comp in comp_list.items():
+        example.addComponent(comp)
+
+    example.GenerateAllConfigs(0)
---
+++ examples/stokes/setup_stokes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Generates input meshes for the Stokes flow example
+# Note: assumes this is run from the build/examples/stokes/ directory
+comp_script="../../../utils/python/box_comp_config.py"
+if [[ ! -f ${comp_script} ]]; then
+    echo "Could not find box_comp_config.py script"
+    exit 1
+fi
+
+# Generate box-channel.comp.h5
+python3 ${comp_script}
+
+# Generate all sample config meshes
+python3 generate_configs.py
+
+mkdir configs/
+mv box-channel.comp.h5 configs/
+
+mkdir configs/samples/
+mv *.h5 configs/samples/
