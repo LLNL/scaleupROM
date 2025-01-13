348fd1b1161cc1d8415cdb6218c837d0b7aa79ea
Seung Whan Chung <seung.chung@austin.utexas.edu>
2025-01-13 13:58:48 -0800

    default option for number of supremizer

--- src/rom_handler.cpp
+++ src/rom_handler.cpp
@@ -175,14 +175,17 @@ void ROMHandlerBase::ParseSupremizerInput(Array<int> &num_ref_supreme, Array<int
    num_supreme.SetSize(numSub);
 
    YAML::Node basis_list = config.FindNode("basis/tags");
+   const int nsup_default = config.GetOption<int>("basis/number_of_supremizer", -1);
 
    BasisTag basis_tag;
    for (int b = 0; b < num_rom_comp; b++)
    {
       basis_tag = GetBasisTagForComponent(b, topol_handler, fom_var_names[1]);
 
-      // By default the same number of velocity basis.
-      num_ref_supreme[b] = config.LookUpFromDict("name", basis_tag.print(), "number_of_supremizer", num_ref_basis[b * num_var], basis_list);
+      // By default the same number of pressure basis.
+      const int nsup0 = (nsup_default > 0) ? nsup_default : num_ref_basis[b * num_var] + 1;
+
+      num_ref_supreme[b] = config.LookUpFromDict("name", basis_tag.print(), "number_of_supremizer", nsup0, basis_list);
    }
 
    for (int m = 0; m < numSub; m++)
